079acc207498701c5945907cab284d2443adbf5e
Coolxer <lukasik4789@gmail.com>
2017-11-17 15:39:22 +0100

    script1.02

--- script.js
+++ script.js
@@ -1,31 +1,11 @@
 var tableName="";
 
+
 $(document).ready(function(){
-	
-	$('.aut').click(function(){
-		tableName = "autorzy";
-	});
-
-	$('.era').click(function(){
-		tableName = "epoki";
-	});
-
-	$('.lit').click(function(){
-		tableName = "dziela";
-	});
-
-	$('.phi').click(function(){
-		tableName = "filozofie_i_kierunki";
-	});
-
-	$('.mot').click(function(){
-		tableName = "motywy";
-	});
-	
-	$('button').click(function(){
+	$('label').click(function(){
+		tableName = $(this).data('table');
 		showTable();
 	});
-	
 });
 
 function showTable()
@@ -33,19 +13,20 @@ function showTable()
 	$.ajax
 	({
 		method: "GET",
-		url: "connection2.php",
+		url: "connection.php",
 		data:  {"tableName": tableName},
 		dataType: "json",
 		converters: {"text json": jQuery.parseJSON}
 	})
 	.done(function(array) 
 	{
-		var myTable= "<table class='table table-bordered table-dark table-hover table-responsive table-striped'><thead><tr class='bg-primary'>";
+		var myTable= "<table class='table table-bordered text-center table-dark table-hover table-responsive table-striped'><thead><tr class='bg-primary'>";
 		
 		for (var i= 0; i < array.names_of_columns.length; i++)
 		{
 			myTable+="<th scope='col'>"+array.names_of_columns[i]+"</th>";
 		}
+		
 		myTable+="</tr></thead><tbody>";
 		
 		for(var i=0; i<array.num_of_rows; i++)
